d4d5c8380e82b2d65294b8616050bc81d6cc0cb4
Alan David Richards <alrichards@incomm.com>
2018-08-01 14:08:47 -0400

    Succesfully created a new project, integrated the old project, & got Jest working.

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
+import Main from '@/components/Main.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    const wrapper = mount(Main)
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
