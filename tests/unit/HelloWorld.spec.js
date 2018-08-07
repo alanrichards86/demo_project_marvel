7f07c038fe6392a0501bebe88bddb2404dc573cd
Alan David Richards <alrichards@incomm.com>
2018-08-07 09:23:48 -0400

    Unit testing working.

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,65 +1,16 @@
-import {shallowMount} from '@vue/test-utils'
 import Main from '@/components/Main.vue'
-import axios from 'axios'
-jest.mock('axios');
+// import Vue from 'vue'
+import {mount} from '@vue/test-utils'
 
-describe('HelloWorld.vue', () => {
-  it('Component does run', t => {
-    // const wrapper = new Vue(Main).$mount();
-    const wrapper = shallowMount(Main);
-    wrapper.vm.$nextTick(() => {
-      console.log('PASSED')
-      expect(wrapper.props()).toEqual({ title: '' })
-      t.pass();
-      t.end();
-    })
-    })
-  })
-
-
-describe('Testing tests', () => {
-  it('True to be true', () => {
-    expect(true).toBeTruthy();
-  })
-})
-
-
-
-
-
-
-
-
-// import { shallowMount } from '@vue/test-utils'
-// import ListOfAvengers from '@/components/ListOfAvengers.vue';
-
-
-// describe('Test should work', () => {
-//   it('True is true', () => {
-//     expect(true).toBeTruthy()
+// describe('Main.vue', () => {
+//   it('Component is mounted and expect name of component to be returned', () => {
+//     const vm = new Vue(Main).$mount()
+//     expect(vm.$options.name).toBe('Main')
 //   })
 // })
-
-// // describe('ListOfAvengers.vue', () => {
-// //     it('Component does run...', () => {
-// //         const wrapper = shallowMount(ListOfAvengers)
-// //         expect(wrapper.is(ListOfAvengers)).toBeTruthy()
-// //     })
-// // })
-
-
-
-
-// // describe('Avengers Component', () => {
-// //     const dom = new jsdom.JSDOM();
-// //     const firstHeading = dom.window.document.querySelector('h1');
-// //     expect(firstHeading.textContent).toContain('Hello');
-
-// // })
-
-// // describe('ListOfAvengers.vue', () => {
-// //     it('Has function addSkill', () => {
-// //         const wrapper = shallowMount(ListOfAvengers)
-// //         expect(wrapper.addSkill()).toBeTruthy();
-// //     })
-// // })
+describe('Main.vue', () => {
+  it('Component is mounted and expect name of component to be returned', () => {
+  const wrapper = mount(Main)
+  expect(wrapper.isVueInstance()).toBe(true)
+  })
+})
--- tests/unit/ListOfAvengers.spec.js
+++ tests/unit/ListOfAvengers.spec.js
@@ -1,17 +1,27 @@
-import {shallowMount} from '@vue/test-utils'
+import Vue from 'vue'
 import ListOfAvengers from '@/components/ListOfAvengers.vue'
 
-describe('HelloWorld.vue', () => {
-  it('Component does run', () => {
-    // const wrapper = new Vue(Main).$mount();
-    const wrapper = shallowMount(ListOfAvengers)
-    expect(wrapper.isVueInstance).toBeTruthy()
+describe('ListOfAvengers.vue', () => {
+    it('Component is mounted and expect name of component to be returned', () => {
+        const vm = new Vue(ListOfAvengers).$mount()
+        expect(vm.$options.name).toBe('ListOfAvengers')
     })
-  })
-
-
-describe('Testing tests', () => {
-    it('True to be true', () => {
-        expect(true).toBeTruthy();
+})
+describe('ListOfAvengers.vue', () => {
+    it('Expect skill variable to be empty', () => {
+        const vm = new Vue(ListOfAvengers).$mount()
+        expect(vm.skill).toBe('')
+    })
+})
+  describe('ListOfAvengers.vue', () => {
+    it('Expect skill variable to not return any string', () => {
+        const vm = new Vue(ListOfAvengers).$mount()
+        expect(vm.skill).not.toBe('string')
+    })
+})
+describe('ListOfAvengers.vue', () => {
+    it('Expect addSkill function to exisit', () => {
+        const vm = new Vue(ListOfAvengers).$mount()
+        expect(vm.addSkill).toBeTruthy()
     })
 })
